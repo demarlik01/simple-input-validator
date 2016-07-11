2fb2f06815c9f495a7c3f160f0cf743f269fb6a4
demarlik01 <hyoseockkim@gmail.com>
2016-07-11 12:03:53 +0900

    combine as one file

--- fields/fields.py
+++ fields/fields.py
@@ -20,3 +20,19 @@ class Field(object):
             raise ValueError('Error: %s is None' % self.name)
         elif self.value == '':
             raise ValueError('Error: %s is empty' % self.name)
+
+
+class IntegerField(Field):
+    """
+    IntegerField
+    """
+    def __init__(self, name, value):
+        super(IntegerField, self).__init__(name, value)
+
+    def check(self):
+        if isinstance(self.value, int) is False:
+            raise ValueError('Error %s is not int' % self.name)
+
+    def validate(self):
+        self.empty_check()
+        self.check()
